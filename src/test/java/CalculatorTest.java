1b19c8b0d840802f73482c7715cd943e1286fcb7
irisation <irisation@tut.by>
2014-12-12 21:22:03 +0300

    my last commit

---
+++ src/test/java/CalculatorTest.java
@@ -0,0 +1,31 @@
+import objects.Calculator;
+import org.testng.Assert;
+import org.testng.annotations.*;
+
+import java.util.List;
+
+public class CalculatorTest {
+    Calculator calc;
+
+    @BeforeMethod
+    public void setup(){
+        calc = new Calculator();
+    }
+
+    @AfterMethod
+    public void afterMethod() {
+        System.out.println("in afterMethod");
+    }
+
+    @Parameters({"value1", "value2","result"})
+    @Test(description = "Test for sum")
+    public void sumTest(int value1, int value2, int result){
+        Assert.assertEquals(result, calc.sum(value1, value2));
+    }
+    @Test(dataProviderClass = DataProviders.class, dataProvider = "calcData")
+    public void dataTest(int value1, int value2, int result){
+            Assert.assertEquals(result, calc.sum(value1, value2));
+
+    }
+
+}
